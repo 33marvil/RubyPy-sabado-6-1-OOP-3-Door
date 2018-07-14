3450e98e433f696d4a4bf3408128a5981a816782
33marvil <martinramirezcruz71@gmail.com>
2018-07-14 14:41:56 -0500

    OPP3 Door version 2

--- door/lib/door.rb
+++ door/lib/door.rb
@@ -1,8 +1,9 @@
 #code goes here
 class Door
-  def initialize(color, size)
+  def initialize(color, size, status="Cerrado")
     @color = color
     @size = size
+    @status = status
   end
 #getter
   def color
@@ -14,17 +15,47 @@ class Door
     # NoMethodError: Don't do that!
     # from (irb):117
     # from /Users/gparsons/.rvm/rubies/ruby-2.1.3/bin/irb:11:in `<main>'
-#getter
+
+#setter
+    def color=(color)
+      @color = color
+    end
+
+#setter
+    def size
+      @size
+    end
+#getter  comportamiento
   def open
-    @open
+    "Business is closed"
   end
-#getter
+  #setter comportamiento
+  def open=(status)
+    "open red door"
+  end
+#getter comportamiento
   def close
     @close
+    "Door is closed"
   end
+  #getter
   def status
+    @status
+  end
+  #setter
+  def status=(status)
+    @status = status
   end
 end
 
 # door_1 = Door.new("green", 5)
-# door_1.color
+# p door_1.color
+#
+# p door_1.size
+# #=> 5
+# p door_1.status
+# #=> "Cerrado"
+# p door_1.open
+# #=> "Business is closed"
+# p door_1.close
+# #=> "Door is closed"
