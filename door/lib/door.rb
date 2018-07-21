45a1a633134309c0b096274f1eea818dcff1b800
33marvil <martinramirezcruz71@gmail.com>
2018-07-21 10:20:49 -0500

    OOP3 class door version 5 refactorizacion y depurado de codigo

--- door/lib/door.rb
+++ door/lib/door.rb
@@ -1,34 +1,35 @@
-#code goes here
+#Door class
 class Door
-  def initialize(color, size, status="Cerrado")#status saignar parametro por default
+  def initialize(color, size, status="Cerrado") #status asignar parametro por default
     @color = color  #variable de instancia.
     @size = size  #variable de instancia.
     @status = status  #variable de instancia.
   end
-#getter
+
+  #getter color
   def color
     raise NoMethodError.new("Metodo no puede usarse!")
-  # rescue => err
-  #   puts err.message
-  end
-  #raise NoMethodError.new("Don't do that!")
+    # rescue => err
+    #   puts err.message
+    #raise NoMethodError.new("Don't do that!")
     # NoMethodError: Don't do that!
     # from (irb):117
     # from /Users/gparsons/.rvm/rubies/ruby-2.1.3/bin/irb:11:in `<main>'
+  end
 
-#setter
-    def color=(color)
-      @color = color
-    end
+  #setter color
+  def color=(color)
+    @color = color
+  end
 
-#setter
-    def size
-      @size
-    end
+  #getter size
+  def size
+    @size
+  end
 
-#comportamiento
-#Si status de door cerrado = Defualt return "Business is closed"
-#Si status de door es Abierto = return "open @color door"
+  #comportamiento
+  #Si status de door cerrado == Defualt return "Business is closed"
+  #Si status de door es Abierto == return "open @color door"
   def open
      @status == "Abierto" ? "open #{@color} door" : "Business is closed" ; #operador ternario
 
@@ -38,9 +39,10 @@ class Door
     #   "open #{@color} door"
     # end
   end
-#comportamiento
-#Si @status de door es "Cerrado" = Default return "Door is closed"
-#Si @status de door es "Abierto" return "close door of 5 meters"
+
+  #comportamiento
+  #Si @status de door es "Cerrado" == Default return "Door is closed"
+  #Si @status de door es "Abierto" return "close door of 5 meters"
   def close
     @status == "Abierto" ? "close door of #{@size} meters" : "Door is closed"; #operador ternario
 
@@ -51,11 +53,12 @@ class Door
     # end
   end
 
-  #getter
+  #getter status
   def status
     @status
   end
-  #setter
+
+  #setter status
   def status=(status)
     @status = status
   end
